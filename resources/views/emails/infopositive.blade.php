617aab528fa457bec01b52f78795c6b16fe49eab
armend Zekjiri <armendzekjiri@gmail.com>
2022-06-30 20:51:53 +0200

    gitignore

--- resources/views/emails/infopositive.blade.php
+++ resources/views/emails/infopositive.blade.php
@@ -2,7 +2,7 @@
     Sehr geehrte/r Kundin/e,
 </p>
 <p>
-    da Ihr Testergebniss POSITIV war, müssen Sie sich sofort für 5 Tage (angefangen ab heute) in Quarantäne begeben. 
+    da Ihr Testergebniss POSITIV war, müssen Sie sich sofort für 5 Tage in Quarantäne begeben. 
     Sie können versuchen, sich am 5. Tag bei uns freizutesten. </p>
 <p>
 
@@ -11,11 +11,11 @@
 
 <p>Für Kontaktpersonen gilt, solange die letzte Impfung/Genesung nicht länger als 90 Tage her ist keine Quarantäne-Pflicht, lediglich ein regelmäßiges Testen an einer Teststelle - für die nächsten 5 Tage.  </p>
         
-<p>Bei weiteren Fragen stehen wir Ihnen gerne telefonisch unter der 015732290120 zur Verfügung. </p>
+<p>Bei weiteren Fragen stehen wir Ihnen gerne telefonisch unter der {{ env('STATION_TEL') }} zur Verfügung. </p>
 
 <p>(Das Gesundheitsamt informieren wir) <br>
     Teilen Sie uns bitte Ihre Telefonnummer mit. 
     </p>
 
     <p>Gute Besserung </p>
-    <p>Ihr Testzentrum Osterfeld</p>
+    <p>{{ env('APP_NAME') }}</p>
