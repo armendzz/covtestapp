30e967e7f4cd79c06c7c53918dff9a85dd496073
armend zekjiri <armendzekjiri@gmail.com>
2022-07-10 19:28:07 +0200

    positive meldung aktualisiert

--- resources/views/emails/infopositive.blade.php
+++ resources/views/emails/infopositive.blade.php
@@ -2,15 +2,15 @@
     Sehr geehrte/r Kundin/e,
 </p>
 <p>
-    da Ihr Testergebniss POSITIV war, müssen Sie sich sofort für 5 Tage in Quarantäne begeben. 
-    Sie können versuchen, sich nach den 5 Tagen bei uns freizutesten. </p>
+da Ihr Testergebniss POSITIV war, müssen Sie sich sofort für 5 Tage in Quarantäne begeben. 
+Sie können versuchen, sich nach den 5 Tagen bei uns freizutesten. </p>
 <p>
 
     <p>Für den Genesenschein ist ein PCR-Test erforderlich. 
-        Wenn Sie einen PCR-Test in Anspruch nehmen möchten, kontaktieren sie bitte zu erst Ihren Hausarzt.</p>
+Wenn Sie einen PCR-Test in Anspruch nehmen möchten, kontaktieren sie bitte zu erst Ihren Hausarzt.</p>
 
-<p>Für Kontaktpersonen gilt, solange die letzte Impfung/Genesung nicht länger als 90 Tage her ist keine Quarantäne-Pflicht, lediglich ein regelmäßiges Testen an einer Teststelle - für die nächsten 5 Tage.  </p>
-<p>Das Freitesten; und Testen der Haushaltsangehörigen ist dabei kostenlos.</p>        
+<p>Für Kontaktpersonen wird lediglich ein regelmäßiges Testen an einer Teststelle - für die nächsten 5 Tage empfohlen.   </p>
+<p>Das Freitesten & Testen der Haushaltsangehötigen ist dabei kostenlos. </p>        
 <p>Bei weiteren Fragen stehen wir Ihnen gerne telefonisch unter der {{ env('STATION_TEL') }} zur Verfügung. </p>
 
 <p>(Das Gesundheitsamt informieren wir) <br>
@@ -19,3 +19,4 @@
 
     <p>Gute Besserung </p>
     <p>{{ env('APP_NAME') }}</p>
+
